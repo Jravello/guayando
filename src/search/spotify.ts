edd0eda47f741469e7a9318554284e81f2ebca64
Josue Israel Ravello Aliste <jravello@trebolit.lan>
2021-09-01 01:24:51 -0400

    busquedas mas precisas

--- src/search/spotify.ts
+++ src/search/spotify.ts
@@ -13,7 +13,36 @@ export async function searchTypeSpotify(
     const search = await searchByUrlAndNameSong(response.name);
     video.push(search);
   } else if (
-    response.type == typesOfArgs.urlPlaylistSP ||
+    response.type == typesOfArgs.urlPlaylistSP
+  ) {
+    const title = response.name;
+    const items = response.tracks.items;
+    const songs = [];
+    const searchEmbed = new Discord.MessageEmbed()
+      .setColor(`#0099ff`)
+      .setDescription(`🎶 Se buscaran las canciones de la Playlist...🎶`);
+    message.channel.send(searchEmbed);
+    const playlistTime = new Discord.MessageEmbed()
+      .setColor(`#0099ff`)
+      .setTitle(`🎶 La Playlist Sera Buscada En Los Registros 🎶`)
+      .setDescription(`Playlist - ${title}`)
+      .setURL(`${search}`);
+    message.channel.send(playlistTime);
+    await Promise.all(
+      items.map(async (item, ind) => {
+        const song = `${item.track.name} - ${item.track.artists[0].name}`;
+        const search = await searchByUrlAndNameSong(song);
+        songs.push(search);
+      })
+    );
+    video.push(...songs);
+    const playlistEmbed = new Discord.MessageEmbed()
+      .setColor(`#0099ff`)
+      .setTitle(`🎶 Playlist Encontrada 🎶`)
+      .setDescription(`Playlist - ${title}`)
+      .setURL(`${search}`);
+    message.channel.send(playlistEmbed);
+  } else if (
     response.type == typesOfArgs.urlAlbumSP
   ) {
     const title = response.name;
@@ -23,12 +52,16 @@ export async function searchTypeSpotify(
       .setColor(`#0099ff`)
       .setDescription(`🎶 Se buscaran las canciones de la Playlist...🎶`);
     message.channel.send(searchEmbed);
-    console.log("Cantidad de canciones =>",items.length)
+    const playlistTime = new Discord.MessageEmbed()
+      .setColor(`#0099ff`)
+      .setTitle(`🎶 La Playlist Sera Buscada En Los Registros 🎶`)
+      .setDescription(`Playlist - ${title}`)
+      .setURL(`${search}`);
+    message.channel.send(playlistTime);
     await Promise.all(
-      items.map(async (item,ind) => {
-        const song = item.track.name;
+      items.map(async (item, ind) => {
+        const song = `${item.name} - ${title}`;
         const search = await searchByUrlAndNameSong(song);
-        console.log(ind+1)
         songs.push(search);
       })
     );
@@ -47,11 +80,16 @@ export async function searchTypeSpotify(
       .setColor(`#0099ff`)
       .setDescription(`🎶 Se buscaran las canciones de la Playlist... 🎶`);
     message.channel.send(searchEmbed);
+    const playlistTime = new Discord.MessageEmbed()
+      .setColor(`#0099ff`)
+      .setTitle(`🎶 La Playlist Sera Buscada En Los Registros 🎶`)
+      .setDescription(`Playlist - ${title}`)
+      .setURL(`${search}`);
+    message.channel.send(playlistTime);
     await Promise.all(
       items.map(async (item) => {
         const song = `${item.name} - ${title}`;
         const search = await searchByUrlAndNameSong(song);
-        console.log("2" ,search)
         songs.push(search);
       })
     );
